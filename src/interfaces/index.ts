be0bb291c543381c18045ef79c50426d44e2e63d
Ennio Visconti <ennio.visconti@gmail.com>
2023-01-02 21:14:43 +0100

    implemented silent fail option

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -3,12 +3,13 @@ import { DotenvParseOutput } from 'dotenv';
 export interface IInput {
   pathToFolder?: string,
   mode?: string,
+  loadMode?: string,
 }
 
 export type IGetInput = () => IInput;
 export type IMain = (input: IInput) => void;
 export type IGeneratePathToFile = (folder: string, mode: string) => string;
-export type IReadFile = (filePath: string) => Promise<string>;
+export type IReadFile = (filePath: string, loadMode: string) => Promise<string>;
 export type IParseEnv = (IParseEnv: string) => DotenvParseOutput;
 export type ISetEnvVariable = (key: [string, string]) => void;
 export type IExportEnvVariables = (env: DotenvParseOutput) => void;
